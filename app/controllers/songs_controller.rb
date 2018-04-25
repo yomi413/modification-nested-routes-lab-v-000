06dac5fd322792ca3ae1ad9c06c60ad82c2cf573
Yomaira Escano <yjescano@gmail.com>
2018-04-25 01:04:11 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -48,7 +48,7 @@ class SongsController < ApplicationController
       if @artist.nil?
         redirect_to artists_path, alert: "Artist not found."
       else
-        @song = artist.songs.find_by(id: params[:id])
+        @song = @artist.songs.find_by(id: params[:id])
         redirect_to artist_songs_path(@artist), alert: "Post not found." if @song.nil?
       end
     else
