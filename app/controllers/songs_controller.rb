1b99e3f5aa198b9f44767ea5cf51e016c8b681bb
Yomaira Escano <yjescano@gmail.com>
2018-04-25 01:03:35 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -45,11 +45,11 @@ class SongsController < ApplicationController
   def edit
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
-      if artist.nil?
+      if @artist.nil?
         redirect_to artists_path, alert: "Artist not found."
       else
         @song = artist.songs.find_by(id: params[:id])
-        redirect_to artist_songs_path(artist), alert: "Post not found." if @song.nil?
+        redirect_to artist_songs_path(@artist), alert: "Post not found." if @song.nil?
       end
     else
       @song = Song.find(params[:id])
